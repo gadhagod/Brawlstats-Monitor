34d5a575bd87a58f9757ddb2d493af57b1707835
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-01-13 13:03:27 -0800

    Add video link

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@ git clone https://github.com/gadhagod/Brawlstats-Monitor # clone the repo
 cd Brawlstats-Monitor # go to cloned repo
 sudo apt-get install arduino # install Arduino
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh  # install arduino cli
-export PATH=$PATH:/home/pi/Brawlstats-Monitor/bin" # add path
-arduino-cli core update-index
+export PATH=$PATH:/home/pi/Brawlstats-Monitor/bin # add path
+arduino-cli config init # initialize CLI configuration
+arduino-cli core update-index # update cores
 arduino-cli core install arduino:avr # install arduino cli core
 arduino-cli lib install "Adafruit SSD1306" # install OLED library
 pip3 install -r requirements.txt # install brawlstats and Jinja
