9a5d1b8f3a370deeb0afdf3cb7a31713396036f4
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-01-12 20:34:51 -0800

    Add README, extend refresh delay

--- main.sh
+++ main.sh
@@ -2,5 +2,5 @@ while :; do
     python3 stats.py
     arduino-cli compile --fqbn arduino:avr:uno sketch
     arduino-cli upload -p "/dev/cu.$(echo $ARDUINO_PORT)" --fqbn arduino:avr:uno sketch
-    sleep 60
+    sleep 120
 done
