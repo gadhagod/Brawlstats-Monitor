9dfda3c84a5745b3605e9a1b88ba0d5c3ee66496
gadhagod <gadhaguy13@gmail.com>
2021-01-13 12:23:15 -0800

    Update setup.sh for linux

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 while :; do
     python3 stats.py
     arduino-cli compile --fqbn arduino:avr:uno sketch
-    arduino-cli upload -p "/dev/cu.$(echo $ARDUINO_PORT)" --fqbn arduino:avr:uno sketch
+    arduino-cli upload -p "/dev/$(echo $ARDUINO_PORT)" --fqbn arduino:avr:uno sketch
     sleep 120
 done
--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
+cd ~ # go to home diretory
+git clone https://github.com/gadhagod/Brawlstats-Monitor # clone the repo
+cd Brawlstats-Monitor # go to cloned repo
 sudo apt-get install arduino # install Arduino
-brew install arduino-cli # install arduino cli
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh  # install arduino cli
+export PATH=$PATH:/home/pi/Brawlstats-Monitor/bin" # add path
+arduino-cli core update-index
 arduino-cli core install arduino:avr # install arduino cli core
 arduino-cli lib install "Adafruit SSD1306" # install OLED library
 pip3 install -r requirements.txt # install brawlstats and Jinja
